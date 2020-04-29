4fb83aa892ecd59fbf7dc1ba09e9f685d5371b7e
kneubel2 <kneubel2@illinois.edu>
2020-04-28 23:37:33 -0500

    added sd card files

---
+++ sd_command.sv
@@ -0,0 +1,85 @@
+module sd_cmd(
+	input logic [7:0] cmd_number, // |= 0x40 (add 64 to cmd number, caller responsible)
+	input logic [31:0] cmd_args, // usually contain address of the data/ length of block
+	input logic [7:0] cmd_crc, // used for some cmds like CMD0
+	input logic clk,
+	input logic start,
+	output logic done,
+	output logic [7:0] response_flags,
+	output logic [31:0] data_transmission
+);
+	int count, next_count;
+	int data_count, next_data_count;
+	logic [7:0] last_byte, next_last_byte;
+	logic [7:0] next_response_flags;
+	logic [31:0] next_data_transmission;
+	
+	
+	always_ff @(posedge clk) begin
+		if start begin
+			count = next_count;
+			data_count = next_data_count;
+			
+			last_byte = next_last_byte;
+			
+			response_flags = next_response_flags;
+			data_transmission = next_data_transmission;
+		end
+		else begin
+			count = 0;
+			data_count = 0;
+			
+			last_byte = 8'h00;
+			
+			response_flags = 8'h00;
+			data_transmission = 32'h00000000;
+		end
+	end
+
+	always_comb begin
+		done = 0;
+		D1 = 0;
+		next_response_flags = response_flags;
+		next_data_transmission = data_transmission;
+	
+		// first byte for cmd number
+		if (count < 8)
+			D1 = cmd_number[count];
+		
+		// next four bytes are for argument
+		elif (count < 40)
+			D1 = cmd_args[count - 8];
+		
+		// last byte is CRC (dummy byte)
+		elif (count < 48)
+			D1 = cmd_crc[count - 40];
+			
+		// then send another dummy byte while waiting for response
+		elif (count < 56)
+			D1 = 0; // <<<< expendable
+			
+		// then we receive a response from D0, flags response
+		elif (count < 64)
+			next_response_flags[count - 56] = D0;
+			
+		// once we are done SENDING cmd...
+		// wait for 0xFE
+		elif (last_byte != 0xFE)
+			next_last_byte = last_byte << 1 || D1;
+		if (last_byte == 0xFE)
+			next_data_count = 0;
+			next_last_byte = 0xFF;
+		// 0xFE has been read, now read data transmission (fixed size 32)
+		if (last_byte == 0xFF and data_count < 32)
+			next_data_count = data_count + 1;
+			next_data_transmission[data_count] = D0;
+		
+		if (data_count >= 32)
+			done = 1;
+		
+		
+	end
+	
+
+
+endmodule
---
+++ sd_init.sv
@@ -0,0 +1,143 @@
+module sd_init(
+	input logic clk,
+	input logic start,
+	output logic done,
+	output logic D1,
+	output logic CS,
+	inout logic D0
+);
+
+	logic [7:0] cmd_number;
+	logic [31:0] cmd_args;
+	logic [7:0] cmd_crc;
+	logic cmd_start, cmd_done;
+	logic [7:0] response_flags;
+	logic [31:0] data_transmission;
+	
+	sd_cmd cmd(
+		.cmd_number, 
+		.cmd_args,
+		.cmd_crc,
+		.clk,
+		.start(cmd_start),
+		.done(cmd_done),
+		.response_flags,
+		.data_transmission,
+		.D1,
+		.D0);
+	
+	always_ff @(posedge clk) begin
+		if (start)
+			state <= next_state;
+		else
+			state <= HALT;
+	end
+	
+	always_comb begin
+		// Default next state logic
+		next_state = state;
+		// Next state logic
+		case (state)
+			HALT: begin
+				next_state = RESET
+			end
+		
+			RESET: begin
+				if (cmd_done) begin
+					if (response_flags == 8'h01)
+						next_state = VOLTAGE_CHECK;
+					cmd_start = 0;
+				end
+			end
+			
+			VOLTAGE_CHECK: begin
+				if (cmd_done) begin
+					if (response_flags == 8'h01 and data_transmission == cmd_args)
+						next_state = INIT1;
+					cmd_start = 0;
+				end
+			end
+			
+			INIT1: begin
+				if (cmd_done) begin
+					if (response_flags == 8'h01)
+						next_state = INIT2;
+					cmd_start = 0;
+				end
+			end
+			
+			INIT2: begin
+				if (cmd_done) begin
+					if (response_flags == 8'h00)
+						next_state = SET_BLOCK_SIZE;
+					else if (response_flags == 8'h01)
+						next_state = INIT1;
+					cmd_start = 0;
+				end
+			end
+			
+			SET_BLOCK_SIZE: begin
+				if (cmd_done) begin
+					if (response_flags == 8'h00)
+						next_state = DONE;
+					cmd_start = 0;
+				end
+			end
+			
+		endcase
+	
+		// Default output values
+		CS = 1;
+		done = 0;
+		cmd_number = 8'h00;
+		cmd_args = 32'h00000000;
+		cmd_crc = 8'h00;
+		// Next output logic
+		case (state)
+			RESET: begin
+				CS = 0;
+				cmd_number = 8'h40 | 8'h00; // CMD0
+				cmd_args = 32'h00000000;
+				cmd_crc = 8'h95;
+				cmd_start = 1'b1;
+			end
+				
+			VOLTAGE_CHECK: begin
+				CS = 0;
+				cmd_number = 8'h40 | 8'h08; // CMD8
+				cmd_args = 32'h000001AA;
+				cmd_crc = 8'h87;
+				cmd_start = 1'b1;
+			end
+			
+			INIT1: begin
+				CS = 0;
+				cmd_number = 8'h40 | 8'h37;  // CMD55
+				cmd_args = 32'h00000000;
+				cmd_crc = 8'h65;
+				cmd_start = 1'b1;
+			end
+			
+			INIT2: begin
+				CS = 0;
+				cmd_number = 8'h40 | 8'h29;  // ACMD41
+				cmd_args = 32'h40000000;
+				cmd_crc = 8'h77;
+				cmd_start = 1'b1;
+			end
+			
+			SET_BLOCK_SIZE: begin
+				CS = 0;
+				cmd_number = 8'h40 | 8'h10; // CMD16
+				cmd_args = 32'h00000004;
+				cmd_crc = 8'hFF;
+				cmd_start = 1'b1;
+			end
+			
+			DONE : begin
+				done = 1'b1;
+			end
+		endcase
+	end
+
+endmodule
